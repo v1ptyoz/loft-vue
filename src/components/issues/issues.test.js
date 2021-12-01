b782f9d5b0a817863b106127a3e19c3cd94e6d3c
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2021-12-02 09:32:26 +1000

    Deleted empty test

--- src/components/issues/issues.test.js
+++ src/components/issues/issues.test.js
@@ -1,10 +1,1 @@
-import { mount } from "@vue/test-utils";
-import issues from "./issues";
 
-it("Меняется надпись при клике", async () => {
-    const wrapper = mount(issues);
-
-    // const span = wrapper.find("span");
-    // await wrapper.find('.toggler').trigger("click");
-    // expect(span.text()).toBe("Hide issues");
-})
